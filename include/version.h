06cc7680b532321dba734ba44145c2ec3bd3e62e
Jordan Marinov <jaisor@gmail.com>
2025-08-30 13:53:57 -0700

    Disabled logging so UART works correctly

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
   #define BUILD_NUMBER "27"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.27 - 2025-08-30 13:42:56.812296"
+  #define VERSION "v0.1.27 - 2025-08-30 13:52:11.634236"
 #endif
 #ifndef VERSION_SHORT
   #define VERSION_SHORT "v0.1.27"
